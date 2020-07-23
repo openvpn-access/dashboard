2c847cb2a3ae4962edd94bc914973cd50ef0f580
Simon Reinisch <simon@reinisch.io>
2020-07-23 16:41:07 +0200

    Prettify VerifyEmail page

---
+++ src/pages/verify-email/Portal.tsx
@@ -0,0 +1,39 @@
+import {cn} from '@utils/preact-utils';
+import {h} from 'preact';
+import {useEffect, useRef, useState} from 'preact/hooks';
+import {JSXInternal} from 'preact/src/jsx';
+import styles from './Portal.module.scss';
+
+type Props<T> = {
+    className?: string;
+    show: keyof T;
+    views: T;
+}
+
+export const Portal = <T extends Record<string, JSXInternal.Element>>(props: Props<T>) => {
+    const [fadeout, setFadeout] = useState(false);
+    const [activeView, setActiveView] = useState(props.show);
+    const mounted = useRef(); // See https://stackoverflow.com/a/53406363
+    let timeout: NodeJS.Timeout | null = null;
+
+    useEffect(() => {
+        if (!mounted.current) {
+            mounted.current = true;
+        } else {
+            setFadeout(true);
+
+            timeout && clearTimeout(timeout);
+            timeout = setTimeout(() => {
+                setActiveView(props.show);
+                setFadeout(false);
+            }, 300);
+        }
+    }, [props.show]);
+
+    return (
+        <div className={cn(styles.portal, props.className)}
+             data-fadeout={fadeout}>
+            {props.views[activeView]}
+        </div>
+    );
+};
--- src/pages/verify-email/VerifyEmail.tsx
+++ src/pages/verify-email/VerifyEmail.tsx
@@ -1,48 +1,73 @@
 import {api} from '@api/index';
 import {Button} from '@components/form/Button';
 import {delayPromise} from '@utils/promises';
-import {FunctionalComponent, h} from 'preact';
+import {Fragment, FunctionalComponent, h} from 'preact';
 import {useState} from 'preact/hooks';
+import {Portal} from './Portal';
 import styles from './VerifyEmail.module.scss';
 
-type State = 'idle' | 'loading' | 'errored' | 'success';
+type State = 'idle' | 'errored' | 'success';
 
 export const VerifyEmail: FunctionalComponent = () => {
-    const [state, setState] = useState<State>('idle');
+    const [loading, setLoading] = useState(false);
+    const [view, setView] = useState<State>('idle');
     const params = new URLSearchParams(location.search);
 
+    // Check if url is invalid
+    if (!params.get('email') || !params.get('token')) {
+        location.href = '/';
+    }
+
     const verify = () => {
-        setState('loading');
+        setLoading(true);
 
         delayPromise(1000, api({
             method: 'POST',
             route: '/users/email/verify',
             data: {token: params.get('token')}
         })).then(() => {
-            setState('success');
+            setView('success');
         }).catch(() => {
-            setState('errored');
+            setView('errored');
+        }).finally(() => {
+            setLoading(false);
         });
     };
 
     return (
-        <div className={styles.verifyEmail}>
-            <h1>Verify {params.get('email')}</h1>
-
-            {state === 'errored' && <p className={styles.error}>An error occured. Please try again later.</p>}
-
-            {state === 'success' && <div className={styles.verified}>
-                <p className={styles.success}>E-Mail successfully verified!</p>
-                <Button text="Take me back"
-                        onClick={() => window.open('/', '_self')}/>
-            </div>}
-
-            {
-                ['idle', 'loading'].includes(state) &&
-                    <Button text="Verify E-Mail!"
-                            loading={state === 'loading'}
-                            onClick={verify}/>
-            }
-        </div>
+        <Portal className={styles.verifyEmail}
+                show={view}
+                views={{
+                    'idle': (
+                        <Fragment>
+                            <h1>Verify {params.get('email')}</h1>
+                            <Button text="Verify E-Mail!"
+                                    loading={loading}
+                                    onClick={verify}/>
+                        </Fragment>
+                    ),
+                    'errored': (
+                        <Fragment>
+                            <div className={styles.errored}>
+                                <div className={styles.msg}>
+                                    <p>An error occured. Please try again later.</p>
+                                    <small>Maybe the verification-token expired. Try logging in and request a new token in this case.</small>
+                                </div>
+                                <a href="/">Back to login</a>
+                            </div>
+                        </Fragment>
+                    ),
+                    'success': (
+                        <Fragment>
+                            <div className={styles.verified}>
+                                <div className={styles.msg}>
+                                    <bc-icon name="checkmark"/>
+                                    <span>E-Mail successfully verified!</span>
+                                </div>
+                                <a href="/">Back to login</a>
+                            </div>
+                        </Fragment>
+                    )
+                }}/>
     );
 };
